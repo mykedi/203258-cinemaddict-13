156c4fcab8bee8a33a454e66d2c7c58b10084f9c
ateplova <ateplova@ozon.ru>
2020-12-27 16:59:10 +0300

    [module3-task2] added empty list and close popup by btn esc

---
+++ src/view/no-film.js
@@ -0,0 +1,29 @@
+import {createElement} from "../utils";
+
+const createNoFilmTemplate = () => {
+  return `<section class="films-list">
+      <h2 class="films-list__title">There are no movies in our database</h2>
+    </section>`;
+};
+
+export default class NoFilm {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoFilmTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
